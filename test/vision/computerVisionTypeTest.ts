da9ce4fe7aa0b1a37b0164de20f3afd32e873d8f
Jeremy Foster <jerfost@microsoft.com>
2018-01-29 17:04:04 -0800

    create an actual test using the vision client

--- test/vision/computerVisionTypeTest.ts
+++ test/vision/computerVisionTypeTest.ts
@@ -1,8 +1,14 @@
 import { computerVision } from "../../src";
+const should = require('should');
+require('dotenv').config();
 
-let computerVisionClient = new computerVision({ apiKey:"", endpoint:"" });
-computerVisionClient.describeImage({
-    body:{},
-    headers:{"Content-Type":'bad value'}
-})
-
+describe('Computer vision type tests', () => {
+    it('should be able to instantiate a vision client', (done) => {
+        let computerVisionClient = new computerVision({
+            apiKey: process.env.NCS_VISION_KEY,
+            endpoint: process.env.NCS_VISION_ENDPOINT
+        });
+        should(computerVisionClient).not.be.undefined();
+        done();
+    });
+});
