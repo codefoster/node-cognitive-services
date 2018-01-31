d76d80dcbe72fa1197e8a2b8aaf0390bd72dffd0
Jeremy Foster <jerfost@microsoft.com>
2018-01-31 10:40:18 -0800

    switch back to config for keys

--- test/vision/computerVisionTypeTest.ts
+++ test/vision/computerVisionTypeTest.ts
@@ -1,12 +1,12 @@
 import { computerVision } from "../../src";
 const should = require('should');
-require('dotenv').config();
+const config = require('../config.js');
 
 describe('Computer vision type tests', () => {
     it('should be able to instantiate a vision client', (done) => {
         let computerVisionClient = new computerVision({
-            apiKey: process.env.NCS_VISION_KEY,
-            endpoint: process.env.NCS_VISION_ENDPOINT
+            apiKey: config.computerVision.apiKey,
+            endpoint: config.computerVision.endpoint
         });
         should(computerVisionClient).not.be.undefined();
         done();
